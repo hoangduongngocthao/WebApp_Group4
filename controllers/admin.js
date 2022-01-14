9a1f7d8f32503b087fc14ba746cd58effaa1b4ad
hoangduongngocthao <thaohdngch190302@fpt.edu.vn>
2022-01-14 11:49:29 +0700

    oke

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { insertObject } = require('../databaseHandler')
+const { insertObject } = require('../db/dbHandler')
 const router = express.Router()
 
 //neu request la: /admin
--- db/dbHandler.js
+++ db/dbHandler.js
@@ -1,13 +1,9 @@
-const {MongoClient,ObjectId} = require('mongodb');
-
-const URL = 'mongodb://localhost:27017';
-const DATABASE_NAME = "GCH0804-ApplicationDev"
-
-async function getDB() {
-    const client = await MongoClient.connect(URL);
-    const dbo = client.db(DATABASE_NAME);
-    return dbo;
-}
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://group4:123@123a@cluster0.zb7jr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true },
+);
 
 async function insertObject(collectionName,objectToInsert){
     const dbo = await getDB();
